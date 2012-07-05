1541263998df445cfb2c3fbfba0e8ae0cec1dea4
Andrea Ceccanti <andrea.ceccanti@gmail.com>
2012-07-05 12:56:31 +0200

    Distribution management in POM

--- src/test/java/org/italiangrid/utils/test/TestServer.java
+++ src/test/java/org/italiangrid/utils/test/TestServer.java
@@ -9,14 +9,25 @@ import javax.servlet.http.HttpServletResponse;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.AbstractHandler;
+import org.italiangrid.utils.https.SSLOptions;
 import org.italiangrid.utils.https.ServerFactory;
 
 public class TestServer {
 
 	
-	TestServer(String hostname, int port) throws Exception{
+	TestServer(String hostname, 
+			int port, 
+			String serverCertFile, 
+			String serverKeyFile,
+			String trustStoreDir) throws Exception {
 		
-		Server s = ServerFactory.newServer(hostname, port);
+		
+		SSLOptions options = new SSLOptions();
+		options.setCertificateFile(serverCertFile);
+		options.setKeyFile(serverKeyFile);
+		options.setTrustStoreDirectory(trustStoreDir);
+		
+		Server s = ServerFactory.newServer(hostname, port, options);
 		s.setHandler(new HelloHandler());
 		s.start();
 		s.join();
@@ -25,14 +36,14 @@ public class TestServer {
 	
 	public static void main(String[] args) throws Exception {
 		
-		if (args.length < 2){
+		if (args.length < 5){
 			
-			System.err.println("Please provide hostname and port as arguments.");
+			System.err.println("Please provide hostname,port,serverCert,serverKey,trustDir as arguments.");
 		}
 		
 		String hostname = args[0];
 		int port = Integer.parseInt(args[1]);
-		new TestServer(hostname, port);
+		new TestServer(hostname, port, args[2], args[3], args[4]);
 		
 	}
 }
